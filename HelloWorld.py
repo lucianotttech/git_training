c9f947055df23e600843cb3591d324e2e5425680
Luciano Curti <luciano.curti@tttech-auto.com>
2021-05-17 16:18:41 +0200

    Add print_numbers function

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,17 @@
-print ('Hello World')
+#from library import Animal
+
+
+def print_numbers():
+    for index in range(10):
+        print(index + 1)
+
+
+print('Hello World')
+
+print_numbers()
+
+'''
+felix = Animal("grey")
+print("Felix's color is: {}".format(felix.color))
+felix.run()
+'''
